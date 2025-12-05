4506032fb631e1b43b1fc3b6a64aeec5e1b7fbd2
vasil <vasilg003@gmail.com>
2025-12-05 17:03:49 +0200

    Create getComputerChoice function

---
+++ javascript.js
@@ -0,0 +1,27 @@
+// Function that returns a random integer in the range [0, max)
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+
+// Function that randomly returns "rock", "paper" or “scissors”.
+function getComputerChoice() {
+    // Get a random integer number between 0 and 2.
+    let randomNumber = getRandomInt(3);
+    let randomChoice;
+
+    switch (randomNumber) {
+        case 0: 
+            randomChoice = "rock";
+            break;
+        case 1:
+            randomChoice = "paper";
+            break;
+        case 2:
+            randomChoice = "scissors";
+            break;
+        default:
+            return "Error. Invalid random number generated!";
+    }
+    return randomChoice;
+}
