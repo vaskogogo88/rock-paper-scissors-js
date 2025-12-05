d988649decedb3da1ab2b3cb034725c979c4a205
vasil <vasilg003@gmail.com>
2025-12-05 17:46:37 +0200

    Return winner in playRound()

--- javascript.js
+++ javascript.js
@@ -27,51 +27,51 @@ function getComputerChoice() {
 
 // Function that will be used to play a single round of rock, paper, scissors.
 function playRound(humanChoice, computerChoice) {
+    // Declare the winner variable
+    let winner;
+    
     // Make the human choice case insensitive.
     humanChoice = humanChoice.toLowerCase();
 
     // Check if the human and computer choices are the same.
     if (humanChoice === computerChoice) {
-        console.log("Tie. Nobody wins.")
-        return;
+        winner = "Tie";
+        return winner;
     }
 
     // Check if the human picked rock.
     if (humanChoice === "rock") {
         if (computerChoice === "paper") {
             computerScore++;
-            console.log("Computer wins.");
+            winner = "Computer";
         } else {
             humanScore++;
-            console.log("You win. Congratulations.");
+            winner = "Human";
         }
-        // Exit the function.
-        return;
     } 
 
     // Check if the human picked paper.
     if (humanChoice === "paper") {
         if (computerChoice === "scissors") {
             computerScore++;
-            console.log("Computer wins.");
+            winner = "Computer";
         } else {
             humanScore++;
-            console.log("You win. COngratulations.")
+            winner = "Human";
         }
-        // Exit the function.
-        return;
     }
 
     // Check if the human picked scissors.
     if (humanChoice === "scissors") {
         if (computerChoice === "rock") {
             computerScore++;
-            console.log("Computer wins.");
+            winner = "Computer";
         } else {
             humanScore++;
-            console.log("You win. Congratulations.");
+            winner = "Human";
         }
     }
+    return winner;
 }
 
 // Function that prompts the user for a choice and then returns it.
@@ -84,11 +84,29 @@ function getUserChoice() {
 function playGame() {
     let userChoice;
     let computerChoice;
+    let winner;
     for (let i = 0; i < 5; i++) {
         userChoice = getUserChoice();
         computerChoice = getComputerChoice();
-        playRound(userChoice, computerChoice);
+        winner = playRound(userChoice, computerChoice);
+        
+        
         console.log(`Round ${i+1}`)
+        // Check who is the winner
+        switch (winner) {
+            case "Tie":
+                console.log("Tie. Nobody wins.");
+                break;
+            case "Computer":
+                console.log("Computer wins. Better luck next time!");
+                break;
+            case "Human":
+                console.log("You win. Congratulations!");
+                break;
+            default:
+                console.log("Error. Did not declare a correct winner.");
+        }
+
         console.log(`User score: ${humanScore}`);
         console.log(`Computer score: ${computerScore}`);
     }
