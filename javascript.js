41f3eb1d5520b42ff160662063d0c92c5c163e90
vasil <vasilg003@gmail.com>
2025-12-05 17:19:09 +0200

    Create playRound function

--- javascript.js
+++ javascript.js
@@ -25,8 +25,61 @@ function getComputerChoice() {
     return randomChoice;
 }
 
+// Function that will be used to play a single round of rock, paper, scissors.
+function playRound(humanChoice, computerChoice) {
+    // Check if the human and computer choices are the same.
+    if (humanChoice === computerChoice) {
+        console.log("Tie. Nobody wins.")
+        return;
+    }
+
+    // Check if the human picked rock.
+    if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            computerScore++;
+            console.log("Computer wins.");
+        } else {
+            humanScore++;
+            console.log("You win. Congratulations.");
+        }
+        // Exit the function.
+        return;
+    } 
+
+    // Check if the human picked paper.
+    if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            computerScore++;
+            console.log("Computer wins.");
+        } else {
+            humanScore++;
+            console.log("You win. COngratulations.")
+        }
+        // Exit the function.
+        return;
+    }
+
+    // Check if the human picked scissors.
+    if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            computerScore++;
+            console.log("Computer wins.");
+        } else {
+            humanScore++;
+            console.log("You win. Congratulations.");
+        }
+    }
+}
+
 // Function that prompts the user for a choice and then returns it.
 function getUserChoice() {
     let userChoice = prompt("Please pick between 'rock', 'paper' and 'scissors'");
     return userChoice;
 }
+
+// Declare the variables that will be used to keep track of the score.
+let computerScore = 0;
+let humanScore = 0;
+
+
+
